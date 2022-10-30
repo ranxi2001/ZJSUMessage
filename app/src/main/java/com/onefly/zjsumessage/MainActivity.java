237d9c314f654c8ba649b212cf995632afd73960
ranxi <ranxi169@163.com>
2022-10-30 09:57:47 +0800

    修改为activity_main为activity_login

--- app/src/main/java/com/onefly/zjsumessage/MainActivity.java
+++ app/src/main/java/com/onefly/zjsumessage/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
