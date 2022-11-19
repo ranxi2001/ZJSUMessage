34b3aabf46d8264e99ca47ebccd5b0575781dfcb
ranxi <ranxi169@163.com>
2022-11-19 20:32:53 +0800

    为activity重新命名，增加addressbook activity

---
+++ app/src/main/java/com/onefly/zjsumessage/AddressbookActivity.java
@@ -0,0 +1,14 @@
+package com.onefly.zjsumessage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddressbookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addressbook);
+    }
+}
