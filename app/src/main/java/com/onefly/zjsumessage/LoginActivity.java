0217fd006991e6950761eb759d75d7f2a0fd1da5
ycy <y3173104782@163.com>
2022-11-20 15:50:11 +0800

    账号的创建，在后台存储账号信息

--- app/src/main/java/com/onefly/zjsumessage/LoginActivity.java
+++ app/src/main/java/com/onefly/zjsumessage/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.onefly.zjsumessage;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseLockedException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -18,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private SharedPreferences.Editor editor;
     private EditText accountedit;
     private EditText passwordedit;
+    private MyDatabaseHelper dbhelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +35,19 @@ public class LoginActivity extends AppCompatActivity {
         rememberPass =findViewById(R.id.checkBox3);
         accountedit=findViewById(R.id.account);
         passwordedit=findViewById(R.id.password);
+        SQLiteDatabase db=dbhelper.getWritableDatabase();
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String account=accountedit.getText().toString();
                 String password=passwordedit.getText().toString();
-                if(account.equals("yuchaoyang")&&password.equals("200206")){
+                Cursor cursor =db.rawQuery("select password from account where account1=?",new String[]{account});
+                String password_true=cursor.getString(cursor.getColumnCount());
+
+                if(passwordedit.getText().toString().equals(password_true)){
+                    Toast.makeText(LoginActivity.this,"欢迎使用商大联系通",Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(LoginActivity.this, MessageActivity.class);
+                    startActivity(intent);
                     editor= pref.edit();
                     if(rememberPass.isChecked()){
                         editor.putBoolean("remember_password",true);
@@ -45,9 +59,9 @@ public class LoginActivity extends AppCompatActivity {
                     }
                     editor.apply();
                 }
-                Toast.makeText(LoginActivity.this,"欢迎使用商大联系通",Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(LoginActivity.this, MessageActivity.class);
-                startActivity(intent);
+                else{
+                    Toast.makeText(LoginActivity.this,"账号或者密码错误！",Toast.LENGTH_SHORT).show();
+                }
             }
         });
         Boolean isremember=pref.getBoolean("checkBox3",false);
@@ -59,6 +73,26 @@ public class LoginActivity extends AppCompatActivity {
         }
 
     }
+    public void register1(View view){
+        Intent intent=new Intent(LoginActivity.this,register.class);
+        startActivity(intent);
+    }
+    public static class MyDatabaseHelper extends SQLiteOpenHelper{
+        public static final String create_account="create table account("+"account1 String primary key autoincrement,"+"password String)";
+        private Context mcontext;
+        public MyDatabaseHelper (Context context , String name, SQLiteDatabase.CursorFactory factory,int version){
+            super(context,name,factory,version);
+            mcontext=context;
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL(create_account);
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/onefly/zjsumessage/register.java
+++ app/src/main/java/com/onefly/zjsumessage/register.java
@@ -2,7 +2,9 @@ package com.onefly.zjsumessage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +14,7 @@ import android.widget.Toast;
 public class register extends AppCompatActivity {
     private Button btn;
     private EditText ed1,ed2,ed3;
-
+    private LoginActivity.MyDatabaseHelper dbhelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +23,21 @@ public class register extends AppCompatActivity {
         ed1=findViewById(R.id.account);
         ed2=findViewById(R.id.password);
         ed3=findViewById(R.id.password1);
-        String account =ed1.getText().toString();
-        String password=ed2.getText().toString();
-        String password1=ed3.getText().toString();
+        dbhelper=new LoginActivity.MyDatabaseHelper(this,"account.db",null,2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(password.equals(password1)){
+                if(ed2.getText().toString().equals(ed3.getText().toString())){
+                    Toast.makeText(register.this, "账号注册成功！", Toast.LENGTH_SHORT).show();
+                    SQLiteDatabase db=dbhelper.getWritableDatabase();
+                    ContentValues values=new ContentValues();
+                    values.put("account1",ed1.getText().toString());
+                    values.put("password",ed2.getText().toString());
+                    db.insert("account",null,values);
+                    values.clear();
+                }
+                else{
                     Toast.makeText(register.this,"两次输入密码不一致，请重新输入！",Toast.LENGTH_SHORT).show();
-                    ed1.setText("");
                     ed2.setText("");
                     ed3.setText("");
                 }
@@ -37,6 +45,7 @@ public class register extends AppCompatActivity {
         });
 
     }
+
     public void register2(View view){
         Intent intent =new Intent(register.this,LoginActivity.class);
         startActivity(intent);
