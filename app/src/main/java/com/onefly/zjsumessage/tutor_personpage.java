cdf37ff50ddb805809f1107571d78ffdd875c55a
ycy <y3173104782@163.com>
2022-12-06 19:01:40 +0800

    老师信息跳转bug修改

--- app/src/main/java/com/onefly/zjsumessage/tutor_personpage.java
+++ app/src/main/java/com/onefly/zjsumessage/tutor_personpage.java
@@ -14,11 +14,14 @@ public class tutor_personpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_personpage);
         btn=findViewById(R.id.button_startchat_fy);
-        Intent intent =new Intent(tutor_personpage.this,ChatActivity.class);
-        String name_fy="封毅";
-        String institute="管工学院";
-        intent.putExtra("institute",institute);
-        intent.putExtra("name",name_fy);
-        startActivity(intent);
+        btn.setOnClickListener(v->{
+            Intent intent =new Intent(tutor_personpage.this,ChatActivity.class);
+            String name_fy="封毅";
+            String institute="管工学院";
+            intent.putExtra("institute",institute);
+            intent.putExtra("name",name_fy);
+            startActivity(intent);
+        });
+
     }
 }
