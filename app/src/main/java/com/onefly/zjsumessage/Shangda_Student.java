57de575ea99c21920b617fcb073248b56384d834
祝橙希 <Zyan_matrix@163.com>
2022-12-08 15:10:53 +0800

    商大学生Shangda_Student部分的编写

---
+++ app/src/main/java/com/onefly/zjsumessage/Shangda_Student.java
@@ -0,0 +1,14 @@
+package com.onefly.zjsumessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shangda_Student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shangda_student);
+    }
+}
